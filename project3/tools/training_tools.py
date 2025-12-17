9b87e1d9c4840d18e510c929a3c2c55175e3777c
britbrat0 <148789427+britbrat0@users.noreply.github.com>
2025-12-17 12:37:49 -0500

    Update training_tools.py

--- project3/tools/training_tools.py
+++ project3/tools/training_tools.py
@@ -72,11 +72,15 @@ def train_model(df, target, task_type, model_name):
     ])
 
     # Train/test split
+    st.write("Splitting data...")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Fit
+     # Fit
+    st.write("Fitting pipeline...")
     pipeline.fit(X_train, y_train)
 
+    st.write("Done training!")
+    
     # Predict
     preds = pipeline.predict(X_test)
 
