682313dc2817f462ab5b8f7d28d513290d33ea5e
britbrat0 <148789427+britbrat0@users.noreply.github.com>
2025-12-17 11:36:22 -0500

    Update model_tools.py

--- project3/tools/model_tools.py
+++ project3/tools/model_tools.py
@@ -1,11 +1,10 @@
 def recommend_models(task_type):
     if task_type == "classification":
-        return [
-            "Logistic Regression",
-            "Random Forest",
-            "XGBoost"
-        ]
-
+    return [
+        "Logistic Regression",
+        "Random Forest",
+        "XGBoost"
+    ]
     if task_type == "regression":
         return [
             "Linear Regression",
